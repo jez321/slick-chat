52c86e17695374460465b5d91399678396f2df47
Jeremy <jezjez@gmail.com>
2020-04-04 10:50:56 +0900

    react-modal setAppElement追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Modal from 'react-modal';
 import { AppWrapper, AppHeader } from './App.styles';
 import { RootState } from './redux/types';
 import ChatInput from './components/ChatInput/ChatInput.component';
 import ChatHistory from './components/ChatHistory/ChatHistory.component';
 import SetUserModal from './components/SetUserModal/SetUserModal.component';
 
+Modal.setAppElement('#root');
 const selectUser = (state: RootState) => state.app.user;
 const App = () => {
   const user = useSelector(selectUser);
